d5f2aa1f9d2703032f920d905d69fe4cafdfc47e
sirazog <alexgherzog@gmail.com>
2023-08-08 19:09:45 -0500

    Formatting Updates, structure for updated functionality of LIGHTLEDs

--- die_roller.ino
+++ die_roller.ino
@@ -7,36 +7,55 @@
 #define PIN_G 8
 #define PIN_ENABLE  9
 
-#define LIGHT_A   0x01
-#define LIGHT_B   0x02
-#define LIGHT_C   0x04
-#define LIGHT_D   0x08
-#define LIGHT_E   0x10
-#define LIGHT_F   0x20
-#define LIGHT_G   0x40
+#define SEG_A   0x01
+#define SEG_B   0x02
+#define SEG_C   0x04
+#define SEG_D   0x08
+#define SEG_E   0x10
+#define SEG_F   0x20
+#define SEG_G   0x40
 
-int onesDigit[] = {LIGHT_A | LIGHT_B | LIGHT_C | LIGHT_D | LIGHT_E | LIGHT_F, LIGHT_B | LIGHT_C, LIGHT_A | LIGHT_B | LIGHT_G | LIGHT_E | LIGHT_D, LIGHT_A | LIGHT_B | LIGHT_G | LIGHT_C | LIGHT_D,
-  LIGHT_F | LIGHT_G | LIGHT_B | LIGHT_C, LIGHT_A | LIGHT_F | LIGHT_G | LIGHT_C | LIGHT_D, LIGHT_F | LIGHT_G | LIGHT_C | LIGHT_D | LIGHT_E, LIGHT_A | LIGHT_B | LIGHT_C,
-  LIGHT_A | LIGHT_B | LIGHT_C | LIGHT_D | LIGHT_E | LIGHT_F | LIGHT_G, LIGHT_A | LIGHT_F | LIGHT_G | LIGHT_B | LIGHT_C};
+//       --A--
+//      |     |
+//      F     B
+//      |     |
+//       --G--
+//      |     |
+//      E     C
+//      |     |
+//       --D--
 
-int tensDigit[] = {};
+int segments[] = { /* 0 */ SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F,
+                   /* 1 */         SEG_B | SEG_C,
+                   /* 2 */ SEG_A | SEG_B         | SEG_D | SEG_E         | SEG_G,
+                   /* 3 */ SEG_A | SEG_B | SEG_C | SEG_D                 | SEG_G,
+                   /* 4 */         SEG_B | SEG_C                 | SEG_F | SEG_G,
+                   /* 5 */ SEG_A         | SEG_C | SEG_D         | SEG_F | SEG_G,
+                   /* 6 */                 SEG_C | SEG_D | SEG_E         | SEG_G,
+                   /* 7 */ SEG_A | SEG_B | SEG_C,
+                   /* 8 */ SEG_A | SEG_B | SEG_C | SEG_D | SEG_E | SEG_F | SEG_G,
+                   /* 9 */ SEG_A | SEG_B | SEG_C                 | SEG_F | SEG_G
+                };
+
+#define TENS_DIGIT  10
+#define ONES_DIGIT  1
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   randomSeed(analogRead(A0));
 
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
-  pinMode(7, OUTPUT);
-  pinMode(8, OUTPUT);
-  pinMode(9, OUTPUT);
+  pinMode(PIN_A, OUTPUT);
+  pinMode(PIN_B, OUTPUT);
+  pinMode(PIN_C, OUTPUT);
+  pinMode(PIN_D, OUTPUT);
+  pinMode(PIN_E, OUTPUT);
+  pinMode(PIN_F, OUTPUT);
+  pinMode(PIN_G, OUTPUT);
+  pinMode(PIN_ENABLE, OUTPUT);
 
-  digitalWrite(PIN_ENABLE, LOW);
-  digitalWrite(PIN_D, HIGH);
+  digitalWrite(PIN_ENABLE, HIGH);
+  digitalWrite(PIN_D, LOW);
 
 }
 
@@ -48,22 +67,34 @@ void loop() {
   delay(100);
 }
 
-void lightLEDs(bool ones, int value) { 
-  int currentSegments;
+void displayNumber(int number) {
+
+}
+
+void lightLEDs(int digit, int number) { 
+  int currentSegments = segments[digit];
+  uint8_t on, off;
   
-  if (ones) {
-    digitalWrite(PIN_ENABLE, LOW);
-    currentSegments = onesDigit[value];
-  } else  { 
-    digitalWrite(PIN_ENABLE, HIGH);
-    currentSegments = tensDigit[value];
+  switch (digit) {
+    case ONES_DIGIT:
+      on = LOW;
+      off = HIGH;
+      digitalWrite(PIN_ENABLE, HIGH);
+      break;
+    case TENS_DIGIT:
+      on = HIGH;
+      off = LOW;
+      digitalWrite(PIN_ENABLE, LOW);
+      break;
+    
+    return;
   }
 
-  digitalWrite(PIN_A, currentSegments & LIGHT_A);
-  digitalWrite(PIN_B, currentSegments & LIGHT_B);
-  digitalWrite(PIN_C, currentSegments & LIGHT_C);
-  digitalWrite(PIN_D, currentSegments & LIGHT_D);
-  digitalWrite(PIN_E, currentSegments & LIGHT_E);
-  digitalWrite(PIN_F, currentSegments & LIGHT_F);
-  digitalWrite(PIN_G, currentSegments & LIGHT_G);
+  digitalWrite(PIN_A, (currentSegments & SEG_A) ? on : off);
+  digitalWrite(PIN_B, (currentSegments & SEG_B) ? on : off);
+  digitalWrite(PIN_C, (currentSegments & SEG_C) ? on : off);
+  digitalWrite(PIN_D, (currentSegments & SEG_D) ? on : off);
+  digitalWrite(PIN_E, (currentSegments & SEG_E) ? on : off);
+  digitalWrite(PIN_F, (currentSegments & SEG_F) ? on : off);
+  digitalWrite(PIN_G, (currentSegments & SEG_G) ? on : off);
 }
